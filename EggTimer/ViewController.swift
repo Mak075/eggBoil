5fbab5639709ab1dd293b0a5a2eed7cb10593d1a
Mak Maid Mulabegovic <mulabegovicmak075@gmail.com>
2022-11-18 15:13:45 +0100

    skoljka jaja 5 maraka

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-
+    @IBOutlet weak var titleText: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    let eggTimes = ["Soft": 3, "Medium": 7, "Hard": 12]
+    var secondsPassed = 1
+    var totalTime = 1
+    var timer = Timer()
+    
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+        timer.invalidate()
+        
+        let hardness = sender.currentTitle!
+        totalTime = eggTimes[hardness]!
+        
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        titleText.text = hardness
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        
+        if secondsPassed < totalTime {
+            
+            secondsPassed += 1
+            progressBar.progress = Float(secondsPassed) / Float(totalTime)
+            print(Float(secondsPassed) / Float(totalTime))
+        }
+        
+        if secondsPassed == totalTime {
+            timer.invalidate()
+            titleText.text = "Done"
+            //break
+        }
+    }
+    
+    @IBAction func restartButton(_ sender: UIButton) {
+        progressBar.progress = 0.0
+        titleText.text = "How do you like your eggs"
+    }
 }
+ 
